b268e513ba5546474488a2b2c53102f2942fb94d
Surve <surve8729@gmail.com>
2024-08-05 15:14:45 +0530

    Implement SecretCode class that generates 4 different colors

---
+++ lib/secret_code.rb
@@ -0,0 +1,12 @@
+class SecretCode
+  attr_reader :code
+  def initialize
+    @colors = ['red', 'green', 'blue', 'yellow', 'white']
+    @code = @colors.sample(4)
+  end
+end
+
+
+# Testing
+# code = SecretCode.new
+# p code.code
